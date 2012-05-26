53002bfa49fab4b6f1043f06893324d629ead766
Stuart Butler <stbutler11@hotmail.com>
2012-05-26 11:14:10 +0100

    Updating to using bootstrap 2.0.1 and Less

--- project/Build.scala
+++ project/Build.scala
@@ -11,8 +11,12 @@ object ApplicationBuild extends Build {
       // Add your project dependencies here,
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings( 
+    		lessEntryPoints <<= baseDirectory(customLessEntryPoints)
     )
+
+    def customLessEntryPoints(base: File): PathFinder = (
+        (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++
+        (base / "app" / "assets" / "stylesheets" * "*.less"))
 
 }
