3e6e69288f28a02c90f6600dd725278422edd7b6
casanovam <michel.casanovaespinosa@gmail.com>
2012-05-27 22:39:47 +0100

    refactoring  Tournament controller

--- app/controllers/TournamentController.scala
+++ app/controllers/TournamentController.scala
@@ -10,9 +10,9 @@ object TournamentController extends Controller {
   
   def start = Action {
       
-     for (i <- 1.to(40)) {
+    /* for (i <- 1.to(40)) {
            User.addUser(new User("player-" + i, "player-" + i, "pw", "e", 1%5, null))
-      }
+      }*/
       if(Team.listTeams().size == 0){
 	Team.generateTeams(User.listUsers())
       }
