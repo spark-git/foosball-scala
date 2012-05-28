357a84bc68bb72f0bc308908d2dffe11d1570b0e
casanovam <michel.casanovaespinosa@gmail.com>
2012-05-28 15:48:12 +0100

    League generation - matches

--- app/controllers/TournamentController.scala
+++ app/controllers/TournamentController.scala
@@ -10,18 +10,25 @@ object TournamentController extends Controller {
   
   def start = Action {
       
-    /* for (i <- 1.to(40)) {
-           User.addUser(new User("player-" + i, "player-" + i, "pw", "e", 1%5, null))
-      }*/
+      if(User.listUsers.size == 0){
+	     for (i <- 1.to(10)) {
+		   User.addUser(new User("player-" + i, "player-" + i, "pw", "e", 1%5, null))
+	      }
+      }
       if(Team.listTeams().size == 0){
 	Team.generateTeams(User.listUsers())
       }
+      League.generateLeague(Team.listTeams());
       
       Ok(html.tournament.start(Team.listTeams()));
   }
   
   def view = Action {
     Ok(html.tournament.start(Team.listTeams()));
+  }
+  
+  def matches = Action {
+    Ok(html.tournament.matches(League.getWeek(0)));
   }
   
 }
--- app/models/Team.scala
+++ app/models/Team.scala
@@ -9,39 +9,48 @@ import play.api.libs.json.JsString
 import play.api.libs.json.Format
 
 case class Team(
-  
+
   player1: User,
   player2: User,
-  name: String
-)
+  name: String)
 
 object Team {
 
-    implicit object TeamFormat extends Format[Team] {
-    
-        def reads(json: JsValue): Team = null
-    	def writes(team: Team): JsValue = JsObject(List("blah" -> JsString(team.name)))
+  implicit object TeamFormat extends Format[Team] {
+
+    def reads(json: JsValue): Team = null
+    def writes(team: Team): JsValue = JsObject(List("blah" -> JsString(team.name)))
+  }
+
+  val TEAMS = scala.collection.mutable.Map[String, Team]()
+
+  def listTeams(): Seq[Team] = {
+    println(TEAMS.values.toList)
+    TEAMS.values.toList;
+  }
+
+  def addTeam(team: Team) = {
+    TEAMS(team.name) = team
+  }
+
+  def generateTeams(players: Seq[User]) {
+
+    for (i <- 0 until players.size / 2) {
+
+      Team.addTeam(new Team(players(i * 2), players((i * 2) + 1), "team-" + i))
+
     }
-  
-    val TEAMS = scala.collection.mutable.Map[String, Team]()
-  
-    def listTeams(): Seq[Team] = {
-	println(TEAMS.values.toList)
-        TEAMS.values.toList;
+  }
+
+  def copyTeams(): ListBuffer[Team] = {
+
+    val list: ListBuffer[Team] = ListBuffer()
+
+    listTeams foreach { i =>
+
+      list += i
     }
-    
-    def addTeam(team:Team) = {
-        TEAMS(team.name) = team
-    }
-    
-    def generateTeams(players: Seq[User]) {
-    
-      println(" players.size:  "+players.size)
-      for(i <-0 until players.size/2){
-	println("i  "+i)
-        Team.addTeam(new Team(players(i*2), players((i*2)+1),"team-"+i))
-        
-      }
-    }
-    
+    list
+  }
+
 }
