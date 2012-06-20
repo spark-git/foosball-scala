5d6d97497fa23a4f8eb45038dbde05e31e65025b
casanovam <michel.casanovaespinosa@gmail.com>
2012-06-20 19:44:32 +0100

    Adding datasource config for cloud db

--- project/Build.scala
+++ project/Build.scala
@@ -9,12 +9,14 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+        "mysql" % "mysql-connector-java" % "5.1.18" 
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings( 
     		lessEntryPoints <<= baseDirectory(customLessEntryPoints)
     )
 
+    
     def customLessEntryPoints(base: File): PathFinder = (
         (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++
         (base / "app" / "assets" / "stylesheets" * "*.less"))
